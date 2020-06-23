09c358839c40ce0107600711d665405218d2154b
Allan Neto <allan.netto@hotmail.com>
2020-06-23 01:56:57 -0300

    Model: Adjust Intention Model

--- backend/src/models/Intention.ts
+++ backend/src/models/Intention.ts
@@ -2,9 +2,13 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+
+import User from '../models/User';
 
 @Entity('intentions')
 class Intention {
@@ -16,6 +20,13 @@ class Intention {
 
   @Column()
   zip_code_end: string;
+
+  @Column()
+  client_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'client_id' })
+  client: User;
 
   @Column()
   lead: boolean;
